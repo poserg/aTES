8a9453874a13e7373bdf7abbfbf93c7f4b0646e3
Sergei Popov <poserg@gmail.com>
2021-11-02 23:42:36 +0300

    Add keycloack support

--- auth/src/main/java/com/github/poserg/ates/auth/AuthApplication.java
+++ auth/src/main/java/com/github/poserg/ates/auth/AuthApplication.java
@@ -2,12 +2,13 @@ package com.github.poserg.ates.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
 public class AuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 
 }
