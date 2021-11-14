416ce6a5777f90588811839a5cc9dd755046a1b9
Sergei Popov <poserg@gmail.com>
2021-11-14 15:10:47 +0300

    WIP Try to add task from UI

--- client-app/src/main/java/com/github/poserg/ates/client/web/controller/TaskClientController.java
+++ client-app/src/main/java/com/github/poserg/ates/client/web/controller/TaskClientController.java
@@ -2,16 +2,20 @@ package com.github.poserg.ates.client.web.controller;
 
 import com.github.poserg.ates.client.web.model.TaskModel;
 import java.util.List;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
+@Slf4j
 public class TaskClientController {
 
     @Value("${task-tracker-service.api.url}")
@@ -32,4 +36,10 @@ public class TaskClientController {
         return "tasks";
     }
 
+    @PostMapping("/saveTask")
+    public String saveTask(@ModelAttribute TaskModel task, BindingResult errors, Model model) {
+        log.debug("Start save: {}", task);
+        model.addAttribute("task", task);
+        return "task";
+    }
 }
