6afd1002fda213946ceb88929964fb8b358facc3
Sergei Popov <poserg@gmail.com>
2021-11-19 19:34:58 +0300

    WIP Add spring cloud

--- auth/src/main/java/com/github/poserg/ates/auth/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.github.poserg.ates.auth.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
